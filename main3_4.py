85e03b8ea38b46b866493103ca27818e370eb987
Adam Grzymkowski <adam.grzymkowski147@gmail.com>
2023-08-09 22:06:22 +0200

    AddEx2.py

--- main3_4.py
+++ main3_4.py
@@ -9,3 +9,22 @@ for name in name_list:
 name_dictionary = dict.fromkeys(name_list, len(name_list))
 print(name_dictionary)
 print('')
+
+#zadanie 2
+
+print('Zadanie 2')
+list_of_numbers = [1, 2, 3, 5, 6, 11, 12, 18, 19, 21]
+list_of_prime_numbers = []
+print('')
+print(f'Liczby z zadania \n{list_of_numbers}')
+print('')
+for number in list_of_numbers:
+  if number == 2 or number == 3:
+    list_of_prime_numbers.append(number)
+  if number % 2 == 0 or number % 3 == 0 or number <= 1:
+    pass
+  else:
+    list_of_prime_numbers.append(number)
+print('Liczby pierwsze')
+print(list_of_prime_numbers)
+print('')
